aed1363700b62e8884c401c100d02ff48b15a3e9
Brittney <brittfort34@gmail.com>
2018-09-11 14:19:55 +0000

    Template Engines

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require('express');
 
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.send('ths is the homepage');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
-    res.send('ths is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:name', function(req, res){
-    res.send('You requested to see a profile with the name of ' + req.params.name);
+     var data = {age: 29, job: 'ninja'};
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
